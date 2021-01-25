cbb15c2effc0f7a0da2ab757f3f1a969b343614e
André Ribeiro <andresribeiro@tutanota.com>
2021-01-25 10:35:41 -0300

    first day

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,51 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import db from "../db.json";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import Widget from "../src/components/Widget";
+import QuizBackground from "../src/components/QuizBackground";
+import Footer from "../src/components/Footer";
+import GitHubCorner from "../src/components/GitHubCorner";
+import { delBasePath } from "next/dist/next-server/lib/router/router";
+
+export const QuizContainer = styled.div`
+  margin: auto 10%;
+  padding-top: 45px;
+
+  width: 100%;
+  max-width: 350px;
+
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Content>
+            <Widget.Header>
+              <h1>Lorem ipsulum</h1>
+            </Widget.Header>
+
+            <p>Lorem ipsulum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <Widget.Header>
+              <h1>Lorem ipsulum</h1>
+            </Widget.Header>
+
+            <p>Lorem ipsulum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/andresribeiro" />
+    </QuizBackground>
+  );
 }
