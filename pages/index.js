ae05607ed0345b4ccba4ec6d8f036eb085492b50
André Ribeiro <andresribeiro@tutanota.com>
2021-01-26 11:01:47 -0300

    second day

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
 import styled from "styled-components";
 import db from "../db.json";
 
 import Widget from "../src/components/Widget";
 import QuizBackground from "../src/components/QuizBackground";
+import QuizLogo from "../src/components/QuizLogo";
 import Footer from "../src/components/Footer";
 import GitHubCorner from "../src/components/GitHubCorner";
 
@@ -21,27 +25,49 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+
+  const [name, setName] = useState("");
+
   return (
     <>
       <Head>
+        <title>Quiz sobre memes</title>
         <meta property="og:image" content={db.bg} />
       </Head>
       <QuizBackground backgroundImage={db.bg}>
         <QuizContainer>
+          <QuizLogo />
           <Widget>
+            <Widget.Header>
+              <h1>Quiz sobre memes</h1>
+            </Widget.Header>
             <Widget.Content>
-              <Widget.Header>
-                <h1>Lorem ipsulum</h1>
-              </Widget.Header>
+              <form
+                onSubmit={(event) => {
+                  event.preventDefault();
 
-              <p>Lorem ipsulum dolor sit amet...</p>
+                  router.push(`/quiz?name=${name}`);
+                }}
+              >
+                <input
+                  placeholder="Diz aí seu nome"
+                  onChange={(event) => {
+                    setName(event.target.value);
+                  }}
+                />
+
+                <button type="submit" disabled={name.length === 0}>
+                  {name.length === 0 ? "Jogar" : `Jogar como ${name}`}
+                </button>
+              </form>
             </Widget.Content>
           </Widget>
 
           <Widget>
             <Widget.Content>
               <Widget.Header>
-                <h1>Lorem ipsulum</h1>
+                <h1>Quizes da galera</h1>
               </Widget.Header>
 
               <p>Lorem ipsulum dolor sit amet...</p>
